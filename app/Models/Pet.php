ddc5aea0b40ec87bded8a0faea5fcb388c180bdf
davesalamangkero28 <dave.salamanca@holychild.edu.ph>
2026-05-14 21:24:53 +0800

    test

--- app/Models/Pet.php
+++ app/Models/Pet.php
@@ -7,4 +7,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('pets', PetController::class);
+Route::resource('pets', PetController::class)->middleware('auth');
+
+require __DIR__.'/auth.php';
